f76f61413f997f97d48859f99e3f75fc06d35570
bobstarr10 <mary.kariuki@strathmore.edu>
2025-02-25 15:56:48 +0300

    created an about us page

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php include('db.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>About Our Organization</h1>
+    </header>
+    <main>
+        <p>We are dedicated to making a difference in the community.</p>
+    </main>
+</body>
+</html>
